0b2cc3a329299505c4dff0700a0a95a6ff55eb5d
tunghoangt <thoang002@citymail.cuny.edu>
2019-11-13 19:32:37 -0500

    Add the slider function

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Slider from './Slider';
+
+const App = () => {
+  return ( <Slider /> )
+}
 
 function App() {
   return (
