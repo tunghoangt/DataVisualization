8739fe6ad30bb3a29b00687d80a39a3dbaecfb51
Thomas Duffy <tduffy000@citymail.cuny.edu>
2019-11-30 16:54:32 -0500

    add aggregation button

--- app/src/App.js
+++ app/src/App.js
@@ -4,17 +4,19 @@ import logo from './logo.svg';
 import './App.css';
 import NewSlider from './Slider';
 import BarChart from './BarChart';
+import AggregationButton from './AggregationButton';
 
 /**
  * Components:
  * 1. Radio button for aggregation (dollars || pounds)
- * 2. Year Filter Slider
+ * 2. Year Filter Slider -- DONE
  * 3. US Map for aggregation visualization and filter by US State
  * 4. BarChart aggregation visualization
 */
 const App = () => {
   return (
     <div className="App">
+      <AggregationButton />
       <NewSlider />
     </div>
   )
