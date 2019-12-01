c3cf31dc34deb1468ed7f1fc093c19a398027206
carlosezmz <39721561+carlosezmz@users.noreply.github.com>
2019-12-01 12:58:28 -0500

    folder
    load the last folder

---
+++ app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Map from './Map';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1> Carlos</h1>
+        <Map />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
