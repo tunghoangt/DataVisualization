23ff22a5d1863a80a6e5833f079c0c59510d8fda
Thomas Duffy <tduffy000@citymail.cuny.edu>
2019-11-30 19:20:59 -0500

    remove unused data cleaner

--- scripts/data_cleaner.py
+++
@@ -1,8 +0,0 @@
-# script used to clean landings in app/src/data/landings.csv
-
-LANDINGS = '../src/data/landings.csv'
-
-with open(LANDINGS, 'r') as f:
-    for line in f:
-        # clean
-        # write 
